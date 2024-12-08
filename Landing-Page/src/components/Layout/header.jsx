34d2ed84655c1a3ae65d37bd557ac89cf2241ec2
sierra-95 <michaelmachohi@gmail.com>
2024-12-08 15:14:26 +0300

    Hamburger menu start

--- Landing-Page/src/components/Layout/header.jsx
+++ Landing-Page/src/components/Layout/header.jsx
@@ -1,11 +1,43 @@
 import React from "react";
 import { Box, Button, Container } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 import {ThemeMuiSwitch} from '../Common/Switch'
 import {useTheme} from '../../context/Theme'
 import {LinknaMali} from '../../assets/images'
 import './css/header.css'
-function Header() {
+
+function Buttons() {
     const { theme, toggleTheme } = useTheme();
+    return(
+        <Box className='flex'>
+            <ThemeMuiSwitch toggleTheme={toggleTheme} checked={theme === 'dark'} />
+            <Button
+                sx={{
+                    color:'inherit',
+                    transition: 'color 0.5s ease',
+                    "&:hover": {
+                        color: "#1976d2"
+                    }
+                }}
+            >Log In</Button>
+            <Button
+                variant="contained"
+                sx={{
+                    marginLeft:'20px',
+                    marginRight:'10px',
+                    backgroundColor:'#22275E',
+                    transition: 'background-color 0.5s ease',
+                    "&:hover": {
+                        backgroundColor: "red"
+                    }
+                }}
+            >Sign Up</Button>
+        </Box>
+    );
+}
+
+function Header() {
+    const isMobile = useMediaQuery("(max-width:1000px)");
     const navItems = ["Home", "Property Listing", "Rentals", "About Us"];
 
     return (
@@ -15,7 +47,13 @@ function Header() {
             <img className="w-[100px]" src={LinknaMali} title="Link na Mali" alt="Link na Mali"/>
             
             {/**Nav */}
-            <Container maxWidth='md' className="div">
+            <div className="hamburger-menu flex-col">
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
+
+            <Container maxWidth='sm' className="header-nav">
                 <ul>
                     {navItems.map((item, index) => (
                         <li className="nav-item" key={index}>{item}</li>
@@ -24,30 +62,9 @@ function Header() {
             </Container>
 
             {/**Login and SignUp */}
-            <Box className='flex'>
-                <ThemeMuiSwitch toggleTheme={toggleTheme} checked={theme === 'dark'} />
-                <Button
-                    sx={{
-                        color:'inherit',
-                        transition: 'color 0.5s ease',
-                        "&:hover": {
-                            color: "#1976d2"
-                        }
-                    }}
-                >Log In</Button>
-                <Button
-                    variant="contained"
-                    sx={{
-                        marginLeft:'20px',
-                        marginRight:'10px',
-                        backgroundColor:'#22275E',
-                        transition: 'background-color 0.5s ease',
-                        "&:hover": {
-                            backgroundColor: "red"
-                        }
-                    }}
-                >Sign Up</Button>
-            </Box>
+            {!isMobile &&(
+                <Buttons />
+            )}
         </Box>
     );
 }
