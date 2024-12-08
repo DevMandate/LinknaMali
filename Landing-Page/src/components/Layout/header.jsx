bdc2370dab5a4e2e75712bb4918a6577c71defbf
sierra-95 <michaelmachohi@gmail.com>
2024-12-08 18:57:47 +0300

    Hamburger Menu Positioning

--- Landing-Page/src/components/Layout/header.jsx
+++ Landing-Page/src/components/Layout/header.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import { Box, Button, Container } from "@mui/material";
 import { useMediaQuery } from "@mui/material";
 import {ThemeMuiSwitch} from '../Common/Switch'
 import {useTheme} from '../../context/Theme'
 import {LinknaMali} from '../../assets/images'
 import './css/header.css'
+import './css/hamburger.css'
 
 function Buttons() {
     const { theme, toggleTheme } = useTheme();
     return(
-        <Box className='flex'>
+        <Box className='flex'
+            sx={{
+            '@media (max-width: 380px)': {
+                flexDirection: 'column-reverse',
+            },
+          }}
+        >
             <ThemeMuiSwitch toggleTheme={toggleTheme} checked={theme === 'dark'} />
             <Button
                 sx={{
@@ -28,7 +35,11 @@ function Buttons() {
                     backgroundColor:'#22275E',
                     transition: 'background-color 0.5s ease',
                     "&:hover": {
-                        backgroundColor: "red"
+                        backgroundColor: "#343A85"
+                    },
+                    '@media (max-width: 380px)': {
+                        marginLeft:'0px',
+                        marginRight:'0px',
                     }
                 }}
             >Sign Up</Button>
@@ -36,32 +47,51 @@ function Buttons() {
     );
 }
 
-function Header() {
-    const isMobile = useMediaQuery("(max-width:1000px)");
+function Nav({isMobile}) {
+    const [menuOpen, setMenuOpen] = useState(false);
     const navItems = ["Home", "Property Listing", "Rentals", "About Us"];
 
-    return (
-        <Box className='flex justify-between items-center w-[100%]'>
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
-            {/**Link na Mali Logo */}
-            <img className="w-[100px]" src={LinknaMali} title="Link na Mali" alt="Link na Mali"/>
-            
-            {/**Nav */}
-            <div className="hamburger-menu flex-col">
+    return(
+        <>
+            <div className="hamburger-menu flex-col" onClick={toggleMenu}>
                 <div className="bar"></div>
                 <div className="bar"></div>
                 <div className="bar"></div>
             </div>
-
-            <Container maxWidth='sm' className="header-nav">
-                <ul>
+            <Container maxWidth='sm' className={`nav-bar ${menuOpen ? 'active' : ''}`}>
+                <ul> 
+                    {isMobile &&(
+                        <Logo size={50}/>
+                    )}
                     {navItems.map((item, index) => (
                         <li className="nav-item" key={index}>{item}</li>
-                    ))}
+                    ))}  
+                    {isMobile &&(
+                        <Buttons />
+                    )}                  
                 </ul>
             </Container>
+        </>
+    );
+}
 
-            {/**Login and SignUp */}
+function Logo({size}){
+    return(
+        <img className={`w-[${size}px]`} src={LinknaMali} title="Link na Mali" alt="LinknaMali Logo"/>
+    )
+}
+
+function Header() {
+    const isMobile = useMediaQuery("(max-width:1000px)");
+
+    return (
+        <Box className='flex justify-between items-center w-[100%]'>
+            <Logo size={100}/>
+            <Nav isMobile={isMobile}/>
             {!isMobile &&(
                 <Buttons />
             )}
